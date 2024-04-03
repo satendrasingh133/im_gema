a27436ccd42bca09a9467049a7928ff893f7814d
satendrasingh133 <165645241+satendrasingh133@users.noreply.github.com>
2024-04-03 17:41:54 +0530

    header

--- src/urls.py
+++ src/urls.py
@@ -3,9 +3,17 @@ from django.urls import path
 from src import views
 
 urlpatterns = [
+    path('inventry/', views.list_inventry, name="list_inventry"),
     path('addinventry/', views.add_inventry, name="add_inventry"),
+    path('update_inventry/<int:inventry_id>/', views.get_inventry_by_id, name="update_inventry"),
+    path('delete_inventry/<int:inventry_id>/', views.delete_inventry, name="delete_inventry"),
+    path('update_inventry_data/', views.update_inventry_data, name="update_inventry_data"),
+
+
     path('adddeviceuser/', views.create_deviceuser, name="add_deviceuser"),
     path('useroverview/', views.user_overview, name="user_overview"),
     path('edit/<int:user_id>/', views.edit_user, name='edit_user'),    
-    path('delete_user/<int:user_id>/', views.delete_deviceuser, name='delete_user')
+    path('delete_user/<int:user_id>/', views.delete_deviceuser, name='delete_user'),
+
+   
 ]
--- src/views.py
+++ src/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, HttpResponse,redirect,get_object_or_404
 from datetime import datetime
 from src.models import Inventry, DeviceUser
+from django.contrib.auth.decorators import login_required
 import re
 
 # Create your views here.
+
+def list_inventry(request):
+    if request.user.is_anonymous:
+        return redirect("/admin")
+    inventries = Inventry.objects.all()
+    return render(request, 'inventry.html', {'inventries': inventries})
+
+def get_inventry_by_id(request, inventry_id):
+    # Retrieve the inventory item by ID or return a 404 error if not found
+    inventry = get_object_or_404(Inventry, pk=inventry_id)
+
+    # Render the template and pass the inventory item to the context
+    return render(request, 'update_inventry.html', {'inventry': inventry})
 def add_inventry(request):
     if request.method == "POST":
         # Get the values from POST data
@@ -20,7 +34,7 @@ def add_inventry(request):
             return render(request, 'add_inventry.html', {'error_message': error_message, 'type':type, 'name':name, 'serial_number':serial_number})
         # Check if the name already exists in the database
         if Inventry.objects.filter(serial_no=serial_number).exists():
-            error_message = "A inventry with the Serial no '{serial_number}' already exists."
+            error_message = f"A inventry with the Serial no '{serial_number}' already exists."
             return render(request, 'add_inventry.html', {'error_message': error_message, 'type':type, 'name':name, 'serial_number':serial_number})
 
         # Create Inventry object if name is not empty
@@ -31,6 +45,33 @@ def add_inventry(request):
         return render(request, 'add_inventry.html', {'success_message': success_message})
 
     return render(request, 'add_inventry.html')
+
+def update_inventry_data(request):
+    if request.method == 'POST':
+        inventry_id = request.POST.get('inventry_id')
+        # Retrieve the inventory item by ID or return a 404 error if not found
+        inventry = get_object_or_404(Inventry, pk=inventry_id)
+        type = request.POST.get('type')
+        name = request.POST.get('name')
+        serial_number = request.POST.get('serial_number')
+
+        # Update the inventory item with the submitted data
+        inventry.type = type
+        inventry.name = name
+        inventry.serial_no = serial_number
+        inventry.updated_at = datetime.today()
+        inventry.updated_by = request.user.username
+        inventry.save()
+        success_message = "Inventry updated successfully."
+        return redirect('list_inventry')
+
+    # Render the form template with the inventory item data
+    return render(request, 'update_inventry.html', {'inventry': inventry})
+
+def delete_inventry(request, inventry_id):
+    inventry = get_object_or_404(Inventry, pk=inventry_id)
+    inventry.delete()
+    return redirect('list_inventry')
 
 def create_deviceuser(request):
     if request.method == 'POST':
@@ -99,13 +140,16 @@ def create_deviceuser(request):
         )  
      
 
-        success_message = "User created successfully."
-        return render(request, 'user_overview.html', {'success_message': success_message})
+        # success_message = "User created successfully."
+        # return render(request, 'user_overview.html', {'success_message': success_message}, )
+        return redirect('user_overview')    
     
     return render(request, 'add_deviceuser.html')
 
 
 def user_overview(request):
+    if request.user.is_anonymous:
+        return redirect("/admin")
 
     deviceuser = DeviceUser.objects.all()
     return render(request, 'user_overview.html', {'deviceuser': deviceuser})  
